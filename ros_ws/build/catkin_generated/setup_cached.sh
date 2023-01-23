87bc67e454ff144ecb8f5d6f446628daecb9a1bf
Winston <winstoncrosby13@gmail.com>
2023-01-23 15:18:53 -0800

    Setup ROS package

--- ros_ws/build/catkin_generated/setup_cached.sh
+++ ros_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/winston/RBE550_Project/ros_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/winston/RBE550_Project/ros_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/winston/RBE550_Project/ros_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/winston/RBE550_Project/ros_ws/src:$ROS_PACKAGE_PATH"
---
+++ ros_ws/build/rbe550_project/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/winston/RBE550_Project/ros_ws/src/rbe550_project
+# Build directory: /home/winston/RBE550_Project/ros_ws/build/rbe550_project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
